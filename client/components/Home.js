234e721c7d7b005ea2a3f6e7c299c855fce0fd30
Mikko Forsström <puhemies@diktaattoriporssi.com>
2014-10-20 17:15:06 +0300

    Lussogran.

--- client/components/Home.js
+++ client/components/Home.js
@@ -4,6 +4,10 @@
 
 var React = require('react');
 var DocumentTitle = require('react-document-title');
+var Jumbotron = require('react-bootstrap/Jumbotron');
+
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Home = React.createClass({
 
@@ -11,9 +15,20 @@ var Home = React.createClass({
         return (
             
             <DocumentTitle title="Dr. Kobros Foundation">
-                <div>
-                    Home
-                </div>
+                
+                <Jumbotron>
+
+                    <div className="container">
+                        <h1>Welcome to the Dr. Kobros Foundation!</h1>
+
+                        <p>
+                            Dr. Kobros Foundation is dedicated to snake oil.
+                        </p>
+
+                        <Link className="btn btn-primary btn-lg" to="recommendations">Want to know more?</Link>
+                    </div>
+                </Jumbotron>
+
             </DocumentTitle>
         );
     }
